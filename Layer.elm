73dedd71aa0996d49a001a9c4622ff22a39e5804
Thomas Stephens <spiralman@gmail.com>
2016-06-25 20:27:26 -0500

    Adding controls for color selection

--- Layer.elm
+++ Layer.elm
@@ -1,8 +1,13 @@
 module Layer exposing ( Model, Msg, init, update, view )
 
+import Color exposing (..)
+import Color.Convert exposing (..)
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick, onInput)
+import Html.Events exposing ( onClick, onInput )
+
+import String exposing ( toFloat )
 
 
 -- Model
@@ -10,12 +15,18 @@ import Html.Events exposing (onClick, onInput)
 type alias Model =
   { name : String
   , editing : Bool
+  , luminanceTolerance : Float
+  , chromaTolerance : Float
+  , selectColor : Color
   }
 
 init : String -> Model
 init layerName =
   { name = layerName
-  , editing = False
+  , editing = True
+  , luminanceTolerance = 0.5
+  , chromaTolerance = 0.5
+  , selectColor = rgb 128 128 128
   }
 
 
@@ -25,6 +36,9 @@ type Msg
   = Rename String
   | StartEditing
   | StopEditing
+  | ChangeLuminanceTolerance Float
+  | ChangeChromaTolerance Float
+  | ChangeSelectColor Color
 
 update : Msg -> Model -> Model
 update msg model =
@@ -35,6 +49,12 @@ update msg model =
         { model | editing = True }
       StopEditing ->
         { model | editing = False }
+      ChangeLuminanceTolerance newTolerance->
+        { model | luminanceTolerance = newTolerance }
+      ChangeChromaTolerance newTolerance->
+        { model | chromaTolerance = newTolerance }
+      ChangeSelectColor newColor ->
+        { model | selectColor = newColor }
 
 
 -- View
@@ -53,4 +73,48 @@ editingView model =
   div []
     [ input [ value model.name, onInput Rename ] []
     , button [ onClick StopEditing ] [ text "Ok" ]
+    , colorControl "Selection" model.selectColor ChangeSelectColor
+    , toleranceControl "Luminance" model.luminanceTolerance ChangeLuminanceTolerance
+    , toleranceControl "Color" model.chromaTolerance ChangeChromaTolerance
     ]
+
+colorControl : String -> Color -> (Color -> Msg) -> Html Msg
+colorControl lbl currentValue msg =
+  label []
+    [ text lbl
+    , input [ value (colorToHex currentValue)
+            , type' "color"
+            , onInput (colorValue msg currentValue)
+            ]
+        []
+    ]
+
+toleranceControl : String -> Float -> (Float -> Msg) -> Html Msg
+toleranceControl lbl currentValue msg =
+  label []
+    [ text lbl
+    , input [ value (toString currentValue)
+            , type' "range"
+            , Html.Attributes.min "0.0"
+            , Html.Attributes.max "1.0"
+            , step "0.01"
+            , onInput (floatValue msg currentValue)
+            ]
+        []
+    ]
+
+floatValue : (Float -> Msg) -> Float -> String -> Msg
+floatValue msg default value =
+  case String.toFloat value of
+      Ok v ->
+        msg v
+      Err _ ->
+        msg default
+
+colorValue : (Color -> Msg) -> Color -> String -> Msg
+colorValue msg default value =
+  case hexToColor value of
+      Just c ->
+        msg c
+      Nothing ->
+        msg default
